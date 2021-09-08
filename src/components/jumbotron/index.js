acf22b5b651615acc2eeb32ebfda2367de9d7653
Ankit <ankit.escalate360@gmail.com>
2021-09-08 23:50:43 +0530

    jumbotron ui

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,35 @@
+import {
+  Container,
+  Inner,
+  Item,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from "./styles/jumbotron";
+
+export const Jumbotron = ({ children, direction = "row", ...restProps }) => (
+  <Item {...restProps}>
+    <Inner direction={direction}>{children}</Inner>
+  </Item>
+);
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
